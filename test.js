d1531704d03db3ba32cd3f67b103dcde0bb788d4
PriyaAR <137667069+PriyaSoftwareDeveloper@users.noreply.github.com>
2024-03-04 16:37:23 +0530

    Simplilearn-Javascript-Practice-Herkey

---
+++ test.js
@@ -0,0 +1,101 @@
+var a = "Priya SDE from Google <br/>";
+document.write(a)
+
+let b = "$12000 <br/>";
+document.write(b);
+
+const c = 12.5; 
+document.write(c)
+
+var a1 = 120;
+var a2 = 300;
+
+var a3 = a1 + a2;
+console.log(a3)
+var a3 = a1 / a2;
+console.log(a3)
+var a3 = a1 % a2;
+console.log(a3)
+var a3 = a1 - a2;
+console.log(a3)
+var a3 = a1 * a2;
+console.log(a3)
+var a3 = a1 + 5;
+console.log(a3)
+a1 += 6;
+console.log(a1)
+
+console.log([100, 200, 300]);
+console.error("Something went wrong");
+console.warn("Warning");
+
+console.log(a1==a2);
+console.log(a1===a1);
+console.log(a1<a2);
+console.log(a1>a2);
+
+var age = 11;
+if (age>=18){
+    console.log("Adult");
+} 
+else {
+    console.log("Not Adult");
+}
+ 
+
+var aa = 100;
+var bb = 200;
+
+if(a>b){
+    console.log("a is greater");
+} else {
+    console.log("b is greater");
+}
+
+var x = 17;
+if(x>=18 && x<=24){
+    console.log("You are eligible!")
+} else {
+    console.log("Not Eligible")
+}
+
+var day=1;
+switch(day){
+    case 0:
+        console.log("Monday");
+        break;
+    case 1:
+        console.log("Tuesday");
+        break;
+    case 2:
+        console.log("Wednesday");
+        break;
+    case 3:
+        console.log("Thursday");
+        break;
+    case 4:
+        console.log("Friday");
+        break;
+    case 5:
+        console.log("Saturday");
+        break;
+    case 6:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Not Valid Choice");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
